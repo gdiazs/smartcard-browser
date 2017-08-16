6a22684ba01f9ef9423a40476be11935556fd40b
neonds <diazgbs@gmail.com>
2017-08-15 19:05:43 -0600

    Spring boot configured

---
+++ smartcard-server/src/main/java/io/gdiazs/smartcardserver/SmartcardServerApplication.java
@@ -0,0 +1,12 @@
+package io.gdiazs.smartcardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcardServerApplication.class, args);
+	}
+}
